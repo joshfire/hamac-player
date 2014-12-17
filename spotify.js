c68381832290cf97f5492655cf0e0914749c7284
Florian Cargoët <florian@joshfire.com>
2014-12-17 17:26:26 +0100

    moved playback code to player.js

--- spotify.js
+++ spotify.js
@@ -1,14 +1,14 @@
 var u       = require('underscore');
 var config  = require('./config.json');
 var spotify = require('node-spotify');
-var adc     = require('./adc.js');
+var adc     = require('./adc');
+var player  = require('./player');
 
-
-// check root
+// check root (required to access GPIOs)
 var id = process.getuid();
 if (id !== 0) {
   process.stderr.write('Must run as root\n');
-  process.exit(1);
+  //process.exit(1);
 }
 
 // check username and password existence
@@ -22,6 +22,8 @@ if (!username || !password) {
 
 // configure node-spotify
 spotify = spotify(config.spotifyOptions);
+// configure player
+player = player(spotify);
 
 // utility function to exit properly
 function exit(code) {
@@ -39,125 +41,43 @@ process.on('SIGINT', function () {
   exit(0);
 });
 
-
-spotify.login(username, password, false, false);
-
-// start the ADC, toggle the player (pause/resume) when the magnet moves
-adc.run(function (err, level) {
+// magnet handler
+function onMagnetValue(err, level) {
   if (err) {
     process.stderr.write(err + '\n');
     return;
   }
-  if (460 < level && level < 480) {
-    if (paused) {
-      console.log('magnet far, playing');
-      togglePlayer();
+  if (config.magnet.low < level && level < config.magnet.high) {
+    if (player.isPaused()) {
+      console.log('Magnet far', level);
+      player.resume();
     }
   } else {
-    if (!paused) {
-      console.log('magnet close, pausing');
-      togglePlayer();
+    if (!player.isPaused()) {
+      console.log('Magnet close', level);
+      player.pause();
     }
   }
-});
-
-var currentTrack;
-
-
-var paused = false;
-function togglePlayer() {
-  if (paused) {
-    spotify.player.resume();
-  } else {
-    spotify.player.pause();
-  }
-  paused = !paused;
 }
 
 spotify.on({
   ready : function () {
-    console.log('Ready');
-
     var playlists = spotify.playlistContainer.getPlaylists();
     var hamacPlaylist = u.findWhere(playlists, {name : config.playlist});
 
-    setup(hamacPlaylist);
+    player.setPlaylist(hamacPlaylist);
+    player.startRandomLoop();
+    // start paused
+    player.pause();
+    // start the ADC, toggle the player (pause/resume) when the magnet moves
+    adc.run(onMagnetValue);
   },
   metadataUpdated : function () {
-    console.log('metadataUpdated', arguments);
   },
   logout : function () {
-    console.log('logout', arguments);
   }
 });
 
-spotify.internal.protos.Track.prototype.play = function () {
-  console.log('Playing ' + this.name);
-  spotify.player.play(this);
-  paused = false;
-};
-
-
-spotify.internal.protos.Playlist.prototype.getRandomTrack = function (trackToAvoid) {
-  var max = this.numTracks;
-  var track;
-  if (max === 0) {
-    return;
-  }
-  if (max === 1) {
-    return this.getTrack(0);
-  }
-  do {
-    var index = Math.floor(Math.random() * max);
-    track = this.getTrack(index);
-  } while (trackToAvoid && track.link === trackToAvoid.link);
-  return track;
-};
-
-spotify.internal.protos.Playlist.prototype.playRandom = function () {
-  var nextTrack = this.getRandomTrack(currentTrack);
-  nextTrack.play();
-  currentTrack = nextTrack;
-};
-
-function logPlaylistInfo(playlist) {
-  console.log('Playlist ' + playlist.name + ' has ' + playlist.numTracks + ' tracks.');
-}
-
-function onTracksAdded(err, playlist, tracks, position) {
-  console.log(tracks.length + ' tracks added.');
-  logPlaylistInfo(playlist);
-}
-
-function onTracksRemoved(err, playlist, trackIndices) {
-  console.log(trackIndices.length + ' tracks removed.');
-  logPlaylistInfo(playlist);
-}
-
-function setup(playlist) {
-  logPlaylistInfo(playlist);
-
-  function makeLogger(name) {
-    return function log() {
-      //console.log(name, arguments);
-    };
-  }
-
-  playlist.on({
-    playlistRenamed     : makeLogger('playlistRenamed'),
-    tracksAdded         : onTracksAdded,
-    tracksMoved         : makeLogger('tracksMoved'),
-    tracksRemoved       : onTracksRemoved,
-    trackCreatedChanged : makeLogger('trackCreatedChanged'),
-    trackSeenChanged    : makeLogger('trackSeenChanged'),
-    trackMessageChanged : makeLogger('trackMessageChanged')
-  });
-
-  spotify.player.on({
-    endOfTrack : function () {
-      playlist.playRandom();
-    }
-  });
-
-  playlist.playRandom();
-}
+// connect to spotify service
+// it triggers everything else
+spotify.login(username, password, false, false);
